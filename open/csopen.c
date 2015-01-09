ce0df434357d584f053a758f2ac93a6196fff0f2
Chien Chung <senmole@163.com>
2015-01-09 18:57:38 +0800

    rewrite open and opend with daemonize

--- open/csopen.c
+++ open/csopen.c
@@ -7,30 +7,16 @@
  */
 int csopen(char *name, int oflag)
 {
-    pid_t pid;
     int len;
     char buf[10];
     struct iovec iov[3];
-    static int fd[2] = { -1, -1 };
+    static int csfd = -1;
 
-    if (fd[0] < 0) {            /* fork/exec our open server first time */
-        if (s_pipe(fd) < 0)
-            err_sys("s_pipe error");
-        if ((pid = fork()) < 0) {
-            err_sys("fork error");
-        } else if (pid == 0) {  /* child */
-            close(fd[0]);
-            if (fd[1] != STDIN_FILENO &&
-                dup2(fd[1], STDIN_FILENO) != STDIN_FILENO)
-                err_sys("dup2 error to stdin");
-            if (fd[1] != STDOUT_FILENO &&
-                dup2(fd[1], STDOUT_FILENO) != STDOUT_FILENO)
-                err_sys("dup2 error to stdout");
-            if (execl("./opend", "opend", (char *)0) < 0)
-                err_sys("execl error");
-        }
-        close(fd[1]);           /* parent */
+    if (csfd < 0) {                /* open connection to conn server */
+        if ((csfd = cli_conn(CS_OPEN)) < 0)
+            err_sys("cli_conn error");
     }
+
     sprintf(buf, " %d", oflag); /* oflag to ascii */
     iov[0].iov_base = CL_OPEN " "; /* string concatenation */
     iov[0].iov_len = strlen(CL_OPEN) + 1;
@@ -39,9 +25,9 @@ int csopen(char *name, int oflag)
     iov[2].iov_base = buf;
     iov[2].iov_len = strlen(buf) + 1; /* +1 for null at end of buf */
     len = iov[0].iov_len + iov[1].iov_len + iov[2].iov_len;
-    if (writev(fd[0], &iov[0], 3) != len)
+    if (writev(csfd, &iov[0], 3) != len)
         err_sys("writev error");
 
     /* read descriptor, returned errors handled by write */
-    return recv_fd(fd[0], write);
+    return recv_fd(csfd, write);
 }
--- opend/opend.h
+++ opend/opend.h
@@ -4,13 +4,25 @@
 #include "apue.h"
 #include <errno.h>
 
+#define CS_OPEN "/home/jian/opend" /* well-known name */
 #define CL_OPEN "open"
 
+extern int debug;       /* nonzero if interactive (not daemon) */
 extern char errmsg[];           /* error message string to return to client */
 extern int oflag;               /* open() flag: O_XXX ... */
 extern char *pathname;          /* of file to open() for client */
 
+typedef struct {                /* one Client struct per connected client */
+    int fd;                     /* fd, or -1 if available */
+    uid_t uid;
+} Client;
+extern Client *client;          /* ptr to malloc'ed array */
+extern int client_size;         /* # entries in client[] array */
+
 int cli_args(int, char **);
-void request(char *, int, int);
+int client_add(int, uid_t);
+void client_del(int);
+void loop(void);
+void request(char *, int, int, uid_t);
 
 #endif // APUE_OPEND_H_
